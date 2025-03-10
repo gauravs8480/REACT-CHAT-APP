8810dd433f3c238d6339fdfe034dd364d526748b
gauravs8480 <gaurav.219301591@muj.manipal.edu>
2025-03-11 00:04:18 +0530

    completed messages routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,26 +1,23 @@
-import express from "express"
-import authRoutes from "./routes/auth.route.js"
+import express from "express";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js"
 import dotenv from "dotenv";
-dotenv.config()
+dotenv.config();
 import { connectDB } from "./lib/db.js";
-import cookieParser from "cookie-parser"
+import cookieParser from "cookie-parser";
 
-
-const app=express();
+const app = express();
 app.use(express.json());
-app.use(cookieParser())
-app.use("/api/auth",authRoutes)
+app.use(cookieParser());
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 app.use((req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
+  console.log(`${req.method} ${req.url}`);
+  next();
 });
-const port=process.env.PORT
+const port = process.env.PORT;
 
-
-app.listen(port,()=>{
-    console.log(`server running on ${port}`);
-    connectDB();
-
-
-    
-})
+app.listen(port, () => {
+  console.log(`server running on ${port}`);
+  connectDB();
+});
